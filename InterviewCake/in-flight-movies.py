0c3bb4f7cdb908597cd4e2c8e869da28900c33de
Nick Dunn <nick.dunn@flosports.tv>
2019-05-18 15:41:01 -0500

    remove dir

--- InterviewCake/in-flight-movies.py
+++
@@ -1,9 +0,0 @@
-
-def has_exact_length(flight_length, movie_lengths):
-    current_movie = movie_lengths.pop()
-
-    for movie_length in movie_lengths:
-        print(movie_length)
-
-
-test_one = has_exact_length(14, [7, 7]))
