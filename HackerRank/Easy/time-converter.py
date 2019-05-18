a8b0ed3c85b51914adbbdec47094cbb802cc5332
Nick Dunn <nick.dunn@flosports.tv>
2019-05-18 14:37:35 -0500

    add unit tests to time-converter.py

--- HackerRank/Easy/time-converter.py
+++ HackerRank/Easy/time-converter.py
@@ -1,3 +1,5 @@
+import unittest
+
 midnight_time = '12:04:22AM'
 noon_time = '12:25:52PM'
 am_time = '05:15:35AM'
@@ -17,8 +19,27 @@ def convert_time_to_military(time):
     return ('%02d:%02d:%02d') % (h, m, s)
 
 
-print(convert_time_to_military(midnight_time))
-print(convert_time_to_military(noon_time))
-print(convert_time_to_military(am_time))
-print(convert_time_to_military(pm_time))
-print(convert_time_to_military(bad_time))
+class ActivityTests(unittest.TestCase):
+    def test_midnight_time(self):
+        """Should change 12 to 00 when it's the midnight hour"""
+        self.assertEqual(convert_time_to_military(midnight_time), '00:04:22')
+
+    def test_noon_time(self):
+        """Should keep 12 as is when it's the noon hour"""
+        self.assertEqual(convert_time_to_military(noon_time), '12:25:52')
+
+    def test_am_time(self):
+        """Should not add 12 hours when it's AM"""
+        self.assertEqual(convert_time_to_military(am_time), '05:15:35')
+
+    def test_pm_time(self):
+        """Should add 12 hours when it's PM"""
+        self.assertEqual(convert_time_to_military(pm_time), '19:05:45')
+
+    def test_bad_time(self):
+        """Should indicate an error when input string doesn't end with AM or PM."""
+        self.assertEqual(convert_time_to_military(bad_time),
+                         'Please provide a valid AM or PM time.')
+
+
+unittest.main()
